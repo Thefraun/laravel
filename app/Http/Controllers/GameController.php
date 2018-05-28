627171cec2a3e09bbcc91532b1d07ee3ae308abd
Logan Fraunfelter <Logan@localhost.localdomain>
2018-05-28 17:59:56 -0400

    added pages and made ltest appear first

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -17,7 +17,7 @@ class GameController extends Controller
 
     public function index()
     {
-    	$games = Game::All();
+    	$games = Game::latest()->paginate(5);
     	return view('games.index', compact('games'));
     }
 
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -37,5 +37,6 @@
         </div>
     </div>
     @endforeach
+    <div class="container">{{ $games->links() }}</div>
 </div>
 @endsection
